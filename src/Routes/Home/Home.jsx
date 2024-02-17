c72061c4108825480f715de420075430237515ad
maxi <maxi_cornalba@hotmail.com.ar>
2024-02-17 10:37:37 -0300

    change API

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,24 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Home.css';
 
 export default function Home() {
-  const [leagueGroups, setLeagueGroups] = useState(null);
-  const token = 'f5a4eb523f66fcc1cf73eb8874eafecf518c433320326f74bdfb54f9006db584';
-  const url = 'https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=f5a4eb523f66fcc1cf73eb8874eafecf518c433320326f74bdfb54f9006db584&from=2024-02-13&to=2024-02-13';
+  const apiKey = '3eb4e79d93msh37313a4eb025e19p14fa92jsn8327a90ee79a';
+  const date = '2024-02-17';
+  const url = `https://api-football-v1.p.rapidapi.com/v3/fixtures?date=${date}`;
 
   const targetLeagues = [
-    { name: "Premier League", league_key: 152 },
-    { name: "La Liga", league_key: 302 },
-    { name: "Serie A", league_key: 207 },
-    { name: "Bundesliga", league_key: 175 },
-    { name: "Ligue 1", league_key: 168 },
-    { name: "Copa de la Liga", league_key: 44 },
-    { name: "Preolímpico Sudamericano SUB-23", league_key: 10608 },
-    { name: "Liga Mexico", league_key: 235 },
-    { name: "Liga Colombia", league_key: 120 },
-    { name: "Liga Paraguay", league_key: 255 },
-    { name: "UEFA Champions League", league_key: 3 }
+    { league: "Copa de la Liga Profesional", id: 1032, country: "Argentina", partidos: {} },
+    { league: "Primera B Metropolitana", id: 131, country: "Argentina", partidos: {} },
+    { league: "Primera Nacional", id: 129, country: "Argentina", partidos: {} },
+    { league: "Premier League", id: 39, country: "England", partidos: {} },
+    { league: "Ligue 1", id: 61, country: "France", partidos: {} },
+    { league: "La Liga", id: 140, country: "Spain", partidos: {} },
+    { league: "Serie A", id: 135, country: "Italy", partidos: {} },
+    { league: "Bundesliga", id: 78, country: "Germany", partidos: {} },
+    { league: "Primera División", id: 265, country: "Chile", partidos: {} },
+    { league: "Liga MX", id: 262, country: "Mexico", },
+    { league: "Primera A", id: 239, country: "Colombia", partidos: {} },
+    { league: "Division Profesional - Apertura", id: 250, country: "Paraguay", partidos: {} }
   ];
+
+  const [leagueMatches, setLeagueMatches] = useState({});
 
   useEffect(() => {
     bringInfo();
@@ -26,43 +29,42 @@ export default function Home() {
 
   const bringInfo = async () => {
     try {
-      const response = await fetch(url);
+
+      const response = await fetch(url, {
+        method: 'GET',
+        params: { live: 'all' },
+        headers: {
+          'X-RapidAPI-Key': apiKey,
+          'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+        }
+      });
+
       if (!response.ok) {
         throw new Error(`Error en la respuesta de la API: ${response.statusText}`);
       }
       const data = await response.json();
-  
-      const groupedLeagues = {};
-  
-      data.result.forEach(match => {
-        const leagueKey = match.league_key;
-        const leagueInfo = targetLeagues.find(league => league.league_key === leagueKey);
-  
-        if (leagueInfo) {
-          // Verificar si la liga ya está en el objeto groupedLeagues
-          if (groupedLeagues[leagueKey]) {
-            // Si existe, agregar el partido a la lista existente
-            groupedLeagues[leagueKey].matches.push(match);
-          } else {
-            // Si no existe, crear una nueva entrada
-            const leagueName = leagueInfo.name;
-  
-            groupedLeagues[leagueKey] = {
-              name: leagueName,
-              matches: [match],
-            };
-          }
-        }
+      const updatedLeagueMatches = {};
+
+      targetLeagues.forEach(targetLeague => {
+        const leagueName = targetLeague.league;
+        const leagueCountry = targetLeague.country; // Nueva línea para obtener el nombre del país
+        const leagueMatches = data.response.filter(match => match.league.id === targetLeague.id);
+        
+        // Utilizamos la propiedad 'league' como clave en updatedLeagueMatches
+        updatedLeagueMatches[leagueName] = {
+          leagueId: targetLeague.id,
+          country: leagueCountry, // Agregamos la propiedad 'country' al objeto
+          matches: leagueMatches,
+        };
       });
-  
-      // Ahora, groupedLeagues contiene las ligas agrupadas por event_country_key
-      setLeagueGroups(groupedLeagues);
+
+      setLeagueMatches(updatedLeagueMatches);
+
     } catch (error) {
       console.log('Error: ', error);
     }
   };
-  console.log(leagueGroups)
-
+  console.log(leagueMatches)
 
   return (
     <div className='container-home'>
@@ -74,3 +76,45 @@ export default function Home() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+          const groupedLeagues = {};
+      
+          data.result.forEach(match => {
+            const leagueKey = match.league_key;
+            const leagueInfo = targetLeagues.find(league => league.league_key === leagueKey);
+      
+            if (leagueInfo) {
+              // Verificar si la liga ya está en el objeto groupedLeagues
+              if (groupedLeagues[leagueKey]) {
+                // Si existe, agregar el partido a la lista existente
+                groupedLeagues[leagueKey].matches.push(match);
+              } else {
+                // Si no existe, crear una nueva entrada
+                const leagueName = leagueInfo.name;
+      
+                groupedLeagues[leagueKey] = {
+                  name: leagueName,
+                  matches: [match],
+                };
+              }
+    
+            }
+          });
+    
+    */
